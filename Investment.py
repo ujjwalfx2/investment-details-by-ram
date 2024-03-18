e0851a2ad91b653457e1c9a81c01cf2278156696
Ujjwal Roy <89298493+ujjwalfx2@users.noreply.github.com>
2024-03-19 00:30:07 +0530

    Update Investment.py
    new change

--- Investment.py
+++ Investment.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(page_title="Investment Details by RAM", layout="wide",initial_sidebar_state="collapsed")
+
 custom_css = """
 <style>
 [data-testid="collapsedControl"] {
@@ -19,7 +20,7 @@ st.markdown(custom_css, unsafe_allow_html=True)
 st.title('Investment Details')
 data = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vScwbSgLHpruvFkDdh_QrjcoheKVnPHyUwOkIKcKgU2r6h6t5SGk5qkuJlXDtwuTM50cQMu4OWliZEp/pub?output=csv')
 
-query = st.text_input("Enter Your PAN Number: (Case Sensitive)")
+query = st.text_input("Enter Your PAN Number: ")
 
 if query:
     mask = data.applymap(lambda x: query in str(x).lower()).any(axis=1)
