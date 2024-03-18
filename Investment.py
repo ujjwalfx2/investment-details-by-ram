48ef17d19b0c32a6320aada9fbb2fcb31392310b
Ujjwal Roy <89298493+ujjwalfx2@users.noreply.github.com>
2024-03-18 18:01:02 +0000

    committed

--- Investment.py
+++ Investment.py
@@ -3,7 +3,18 @@ import pandas as pd
 import numpy as np
 import requests
 
-st.set_page_config(page_title="Investment Details by RAM", layout="wide")
+st.set_page_config(page_title="Investment Details by RAM", layout="wide",initial_sidebar_state="collapsed")
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
 st.title('Investment Details')
 data = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vScwbSgLHpruvFkDdh_QrjcoheKVnPHyUwOkIKcKgU2r6h6t5SGk5qkuJlXDtwuTM50cQMu4OWliZEp/pub?output=csv')
 
